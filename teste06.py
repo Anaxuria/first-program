264072a265354833c567d5aef7c81325fb78ce37
anaxuria <anajuliagarcia.ajg@gmail.com>
2023-09-07 20:20:43 -0300

    a

---
+++ teste06.py
@@ -0,0 +1,174 @@
+# Conjuntos
+
+# não se repete, não é garantido que a ordem se mantenha.
+
+numeros = set([1, 2, 3, 1, 3, 4])
+print(numeros)  # {1, 2, 3, 4}
+
+letras = set("abacaxi")
+print(letras)  # {"b", "a", "c", "x", "i"}
+
+carros = set(("palio", "gol", "celta", "palio"))
+print(carros)  # {"gol", "celta", "palio"}
+
+# acessar os dados, tem que converter ele para uma lista
+
+numeros = {1, 2, 3, 2}
+
+numeros = list(numeros)
+
+print(numeros[0])
+
+# iterar 
+
+carros = {"gol", "celta", "palio"}
+
+for carro in carros:
+    print(carro)
+
+for indice, carro in enumerate(carros):
+    print(f"{indice}: {carro}")
+
+# {}union - União
+
+conjunto_a = {1, 2}
+conjunto_b = {3, 4}
+
+resultado = conjunto_a.union(conjunto_b)
+print(resultado)
+
+# {}.intersection - Intersecção 
+
+conjunto_a = {1, 2, 3}
+conjunto_b = {2, 3, 4}
+
+resultado = conjunto_a.intersection(conjunto_b)
+print(resultado)
+
+# {}.difference - União-conjunto x
+
+conjunto_a = {1, 2, 3}
+conjunto_b = {2, 3, 4}
+
+resultado = conjunto_a.difference(conjunto_b)
+print(resultado)
+
+resultado = conjunto_b.difference(conjunto_a)
+print(resultado)
+
+# {}.symmetric_difference - União-Intersecção
+
+conjunto_a = {1, 2, 3}
+conjunto_b = {2, 3, 4}
+
+resultado = conjunto_a.symmetric_difference(conjunto_b)
+print(resultado)
+
+# {}.issubset - conjunto x está contido no conjunto y
+
+conjunto_a = {1, 2, 3}
+conjunto_b = {4, 1, 2, 5, 6, 3}
+
+resultado = conjunto_a.issubset(conjunto_b)  # True
+print(resultado)
+
+resultado = conjunto_b.issubset(conjunto_a)  # False
+print(resultado)
+
+# {}.issuperset - conjunto y está contido no conjunto x
+
+conjunto_a = {1, 2, 3}
+conjunto_b = {4, 1, 2, 5, 6, 3}
+
+resultado = conjunto_a.issuperset(conjunto_b)  # False
+print(resultado)
+
+resultado = conjunto_b.issuperset(conjunto_a)  # True
+print(resultado)
+
+# {}.isdisjoint - quando não tem uma intersecção entre os conjuntos
+
+conjunto_a = {1, 2, 3, 4, 5}
+conjunto_b = {6, 7, 8, 9}
+conjunto_c = {1, 0}
+
+resultado = conjunto_a.isdisjoint(conjunto_b)  # True
+print(resultado)
+
+resultado = conjunto_a.isdisjoint(conjunto_c)  # False
+print(resultado)
+
+# {}.add
+
+sorteio = {1, 23}
+
+sorteio.add(25)  # {1, 23, 25}
+print(sorteio)
+
+sorteio.add(42)  # {1, 23, 25, 42}
+print(sorteio)
+
+sorteio.add(25)  # {1, 23, 25, 42}
+print(sorteio)
+
+# {}.clear - transforma em conjunto vazio
+
+sorteio = {1, 23}
+
+print(sorteio)  # {1,23}
+
+sorteio.clear()
+
+print(sorteio)  # {}
+
+# {}.copy
+
+sorteio = {1, 23}
+
+print(sorteio)  # {1, 23}
+
+sorteio.copy()
+
+print(sorteio)  # {1, 23}
+
+# {}.discard
+
+numeros = {1, 2, 3, 1, 2, 4, 5, 5, 6, 7, 8, 9, 0}
+
+print(numeros)  # {1, 2, 3, 4, 5, 6, 7, 8, 9, 0}
+
+numeros.discard(1)
+numeros.discard(45)
+
+print(numeros)  # {2, 3, 4, 5, 6, 7, 8, 9, 0}
+
+# {}.pop - ele é ao contrario da lista
+
+numeros = {1, 2, 3, 1, 2, 4, 5, 5, 6, 7, 8, 9, 0}
+
+print(numeros)  # {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+print(numeros.pop())  # 0
+print(numeros.pop())  # 1
+print(numeros)  # {2, 3, 4, 5, 6, 7, 8, 9}
+
+# {}.remove
+# diferença entre ele e o discard é que se colocar um valor que não existe dá erro, o discard não.
+
+numeros = {1, 2, 3, 1, 2, 4, 5, 5, 6, 7, 8, 9, 0}
+
+print(numeros)  # {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+print(numeros.remove(0))  # 0
+print(numeros)  # {1, 2, 3, 4, 5, 6, 7, 8, 9}
+
+# {}.len - dar o tamanho do conjunto
+
+numeros = {1, 2, 3, 1, 2, 4, 5, 5, 6, 7, 8, 9, 0}
+
+print(len(numeros))  # 10
+
+# {}.in - verificar se o elemento está no conjunto.
+
+numeros = {1, 2, 3, 1, 2, 4, 5, 5, 6, 7, 8, 9, 0}
+
+print(1 in numeros)  # True
+print(10 in numeros)  # False
